4d00c101adb2cfd6aeaab4b2798548968a01772c
Eyalg0 <kingshorti@gmail.com>
2022-04-16 17:15:37 +0300

    add preflint test

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+GLOBAL_VAR = 2
+
 
 def print_hi(name):
     """
@@ -13,7 +15,11 @@ def print_hi(name):
     """
     # Use a breakpoint in the code line below to debug your script.
     your_age = 43
+    local_var = GLOBAL_VAR
     print(f"Hi, {name} {your_age}")  # Press Ctrl+F8 to toggle the breakpoint.
+    some_list = (1, 2, 3, 4)
+    for some_item in some_list:
+        print(some_item**local_var)
 
 
 # Press the green button in the gutter to run the script.
