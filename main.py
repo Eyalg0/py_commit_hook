da89f8cccdf979afdbeea9babcd0fac95604932f
Eyalg0 <kingshorti@gmail.com>
2022-04-16 17:07:24 +0300

    lint and black test

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ def print_hi(name):
     :return:
     """
     # Use a breakpoint in the code line below to debug your script.
-    age = 43
-    print(f'Hi, {name} {age}')  # Press Ctrl+F8 to toggle the breakpoint.
+    your_age = 43
+    print(f"Hi, {name} {your_age}")  # Press Ctrl+F8 to toggle the breakpoint.
 
 
 # Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
+    print_hi("PyCharm")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
